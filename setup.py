dd9e51ca085dbf1293b3b3d3686b3fdd5e623059
andrelcunha <deko81@gmail.com>
2017-06-28 18:19:57 -0300

    Corrected minor bugs Version:0.2.1

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from setuptools import find_packages
 
 with open('README.md') as f:
     readme = f.read()
@@ -8,14 +12,16 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(
-    name='tsm_seg_bkp_python',
-    version='0.2.1',
-    description='Tool to backup a path using TSM, in a segmented way.',
-    long_description=readme,
-    author='Andre Luis da Cunha',
-    author_email='andre.l.cunha@live.com',
-    url='https://github.com/andrelcunha/tsm_seg_bkp_python',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+config = {
+    'name': 'tsm_seg_bkp_python',
+    'version': '0.2.1',
+    'description': 'Tool to backup a path using TSM, in a segmented way.',
+    'long_description': readme,
+    'author': 'Andre Luis da Cunha',
+    'author_email': 'andre.l.cunha@live.com',
+    'url': 'https://github.com/andrelcunha/tsm_seg_bkp_python',
+    'license': license,
+    'packages': find_packages(exclude=('tests', 'docs'))
+}
+
+setup(**config)
