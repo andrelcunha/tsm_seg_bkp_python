55bfbcc4727363abbe8633459475aaa843d10a6a
andrelcunha <deko81@gmail.com>
2017-08-29 16:05:08 -0300

    Some functions were moved to tools.

--- tsm_seg_bkp/__init__.py
+++ tsm_seg_bkp/__init__.py
@@ -1,5 +1,6 @@
 import tsm_seg_bkp.bkp_nas_seg
 import tsm_seg_bkp.level_listdir
 import tsm_seg_bkp.path_generator
+import tsm_seg_bkp.tools
 
-__all__ = ['bkp_nas_seg', 'level_listdir', 'path_generator']
+__all__ = ['bkp_nas_seg', 'level_listdir', 'path_generator', 'tools']
---
+++ tsm_seg_bkp/expresso/__init__.py
@@ -0,0 +1,3 @@
+import tsm_seg_bkp.expresso.generate_config_file
+
+__all__ = ['generate_config_file']
