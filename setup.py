77b9c01b214187d47804ebed5fdcc62a86f10c87
andrelcunha <deko81@gmail.com>
2017-10-05 16:53:34 -0300

    Fixed smaller issues

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as f:
 
 config = {
     'name': 'tsm_seg_bkp_python',
-    'version': '0.4.21',
+    'version': '0.4.22',
     'description': 'Tool to backup a path using TSM, in a segmented way.',
     'long_description': readme,
     'author': 'Andre Luis da Cunha',
